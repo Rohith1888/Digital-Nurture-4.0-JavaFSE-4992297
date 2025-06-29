4785a3fb21dd598e1c2a7f59f09f9dd172eb87f4
Rohith1888 <gopisettyrohith18@gmail.com>
2025-06-29 17:03:51 +0530

    completed SLJ4 Logging Exercise

---
+++ Week2/SLJ4_Logging_Exercise/SLJ4Logging/src/test/java/com/cognizant/SLJ4Logging/AppTest.java
@@ -0,0 +1,19 @@
+package com.cognizant.SLJ4Logging;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
